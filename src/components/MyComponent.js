59364fd64af3af9761ed9c287c5d651d49433467
Duykiller04 <haijano04@gmail.com>
2025-08-09 16:00:06 +0700

    state

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -5,12 +5,17 @@ import React from "react";
 
 class MyComponent extends React.Component {
 
+    state = {
+        name: 'Duy',
+        address: 'HN',
+        age: 21
+    }
+
     //JSX
     render(){
         return (
             <div>
-                my fist component 
-                {Math.random()}
+                my name is {this.state.name} and I'm from {this.state.address}
             </div>
         );
     }
