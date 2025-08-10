7af86950a497f9169e471ec3ce30cc0e5af990e8
Duykiller04 <haijano04@gmail.com>
2025-08-10 16:45:37 +0700

    route dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.scss';
 import Headers from './components/Header/Header';
+import { Link } from "react-router-dom";
 
 const App = () => {
   return(
     <div className='app-container'>
       <Headers/>
+      <div>
+        test link
+        <div>
+          <button className='btn btn-primary'>
+            <Link to="/users">
+              go to user page
+            </Link>
+          </button>
+          <button className='btn btn-danger'>
+            <Link to="/admins">
+              go to admin page
+            </Link>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
