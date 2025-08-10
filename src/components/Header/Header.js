472b31243aa5032cd9dbcb7608015621518e0fa8
Duykiller04 <haijano04@gmail.com>
2025-08-10 23:21:40 +0700

    sửa lại css homepage, nav

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,27 +1,41 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className="custom-navbar">
       <Container>
-        <NavLink to="/" className='navbar-brand'>KhuatDuy</NavLink>
+        <NavLink to="/" className="navbar-brand fw-bold fs-4">
+          KhuatDuy
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <NavLink to="/" className='nav-link'>Home</NavLink>
-            <NavLink to="/users" className='nav-link'>User</NavLink>
-            <NavLink to="/admins" className='nav-link'>Admin</NavLink>
+          <Nav className="me-auto ms-4">
+            <NavLink to="/" className="nav-link">
+              Home
+            </NavLink>
+            <NavLink to="/users" className="nav-link">
+              User
+            </NavLink>
+            <NavLink to="/admins" className="nav-link">
+              Admin
+            </NavLink>
           </Nav>
           <Nav>
+            <div className="d-flex align-items-center">
+              <button className="btn btn-outline-dark login-link me-3">
+                Login
+              </button>
+              <button className="btn btn-dark rounded-pill px-4">
+                Sign up
+              </button>
+            </div>
             <NavDropdown title="Settings" id="basic-nav-dropdown">
               <NavDropdown.Item>Login</NavDropdown.Item>
-              <NavDropdown.Item>
-                Logout
-              </NavDropdown.Item>
+              <NavDropdown.Item>Logout</NavDropdown.Item>
               <NavDropdown.Item>Profile</NavDropdown.Item>
             </NavDropdown>
           </Nav>
@@ -29,6 +43,6 @@ const Header = () => {
       </Container>
     </Navbar>
   );
-}
+};
 
 export default Header;
--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -6,6 +6,13 @@ const HomePage = () => {
             <video autoPlay muted loop>
                 <source src={videoHomePage} type="video/mp4"/>
             </video>
+            <div className='homepage-content'>
+                <div className='title-1'>Get to know your customers with forms worth filling out</div>
+                <div className='title-2'>Collect all the data you need to understand customers with forms designed to be refreshingly different.</div>
+                <div className='title-3'>
+                    <button className='btn btn-dark'>Get started—it's free</button>
+                </div>
+            </div>
         </div>
     )
 }
